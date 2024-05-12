21cf40bbba557d9eb41cf65d6315d3b96fc9c687
Rishabh Saxena <40590709+saxena-rishabh@users.noreply.github.com>
2024-05-12 11:33:53 +0530

    Time: 107 ms (45.37%), Space: 17.4 MB (57.93%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -3,9 +3,10 @@ class Solution:
         start_pointer = 0
         end_pointer = len(numbers)-1
         while start_pointer < end_pointer:
-            if numbers[start_pointer] + numbers[end_pointer] == target:
+            pair_sum = numbers[start_pointer] + numbers[end_pointer]
+            if pair_sum == target:
                 return [start_pointer + 1, end_pointer + 1]
-            elif numbers[start_pointer] + numbers[end_pointer] > target:
+            elif pair_sum > target:
                 end_pointer -= 1
-            elif numbers[start_pointer] + numbers[end_pointer] < target:
+            elif pair_sum < target:
                 start_pointer += 1
