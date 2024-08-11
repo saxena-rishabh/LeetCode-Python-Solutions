3e1558ef4e76af7859e00d6b63e615ba09ab9ed6
Rishabh Saxena <40590709+saxena-rishabh@users.noreply.github.com>
2024-08-11 12:03:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,15 +1,11 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         dic = {}
-
-        if len(s) != len(t):
-            return False
-        
         for i in range(len(s)):
-            if s[i] in dic:
-                if dic[s[i]] != t[i]:
-                    return False
+            if s[i] not in dic:
+                dic[s[i]] = (s[i], t[i])
             else:
-                dic[s[i]] = t[i] 
+                if dic[s[i]][1] != t[i]:
+                    return False
+            
         return True
-        
