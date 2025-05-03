e22b73584904871b6fb346c8aa903c01f1c156ea
Rishabh Saxena <40590709+saxena-rishabh@users.noreply.github.com>
2025-05-03 12:58:55 +0530

    Time: 31 ms (2.68%), Space: 16.6 MB (100%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,21 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        nums1_end_index = m - 1
-        nums2_end_index = n - 1
-        global_end_index = m + n - 1
 
-        while nums1_end_index >= 0 and nums2_end_index >= 0:
-            if nums1[nums1_end_index] > nums2[nums2_end_index]:
-                nums1[global_end_index] = nums1[nums1_end_index]
-                nums1_end_index -= 1
+        i = m - 1
+        j = n - 1
+        k = m + n -1
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+                k -= 1
             else:
-                nums1[global_end_index] = nums2[nums2_end_index]
-                nums2_end_index -= 1
-            global_end_index -= 1
-
-        # If there are remaining elements in nums2
-        while nums2_end_index >= 0:
-            nums1[global_end_index] = nums2[nums2_end_index]
-            nums2_end_index -= 1
-            global_end_index -= 1
+                nums1[k] = nums2[j]
+                j -= 1
+                k -= 1
+        
+        while i >=0:
+            nums1[k] = nums1[i]
+            i -= 1
+            k -= 1
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
